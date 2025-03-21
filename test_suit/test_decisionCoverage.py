f7d7d7701a5671b6294e8d6db07916ac2db6ae4e
fferdous <fferdous@umass.edu>
2025-03-21 14:23:55 -0400

    added test cases to statementCoverage and decisionCoverage, specifically for equilateral, isosceles, and scalene triangles.

--- test_suit/test_decisionCoverage.py
+++ test_suit/test_decisionCoverage.py
@@ -12,3 +12,18 @@ def test_negative_sides():
   assert Triangle.classify(4,-4,4) == Triangle.Type.INVALID
   assert Triangle.classify(1,1,-1) == Triangle.Type.INVALID
   #tests that triangle with one or more negative sides is invalid triangle
+
+def test_scalene():
+  assert Triangle.classify(3, 4, 5) == Triangle.Type.SCALENE
+  #tests that triangle is scalene
+
+def test_isosoceles():
+  assert Triangle.classify(5, 5, 8) == Triangle.Type.ISOSCELES
+  assert Triangle.classify(5, 8, 5) == Triangle.Type.ISOSCELES
+  assert Triangle.classify(8, 5, 5) == Triangle.Type.ISOSCELES
+  #tests that triangle is isosceles (for different values of a,b,c to cover all decision nodes)
+
+def test_equilateral():
+  assert Triangle.classify(3, 3, 3) == Triangle.Type.EQUILATERAL
+  #tests that triangle is equilateral (a=b=c)
+
--- test_suit/test_statementCoverage.py
+++ test_suit/test_statementCoverage.py
@@ -12,3 +12,19 @@ def test_negative_sides():
   assert Triangle.classify(-4,-4,4) == Triangle.Type.INVALID
   assert Triangle.classify(-1,-1,-1) == Triangle.Type.INVALID
   #tests that triangle with one or more negative sides is invalid triangle
+
+def test_scalene():
+  assert Triangle.classify(3, 4, 5) == Triangle.Type.SCALENE
+  #tests that triangle is scalene
+
+def test_isosceles():
+  assert Triangle.classify(4, 4, 6) == Triangle.Type.ISOSCELES
+  assert Triangle.classify(3, 5, 3) == Triangle.Type.ISOSCELES
+  assert Triangle.classify(7, 5, 5) == Triangle.Type.ISOSCELES
+  #tests that triangle is isosceles (for different values of a,b,c)
+
+def test_equilateral():
+  assert Triangle.classify(3, 3, 3) == Triangle.Type.EQUILATERAL
+  #tests that triangle is equilateral (a=b=c)
+
+
