355dcfe5bb6a7409b0164adf27a1e6a391376c7c
fferdous <fferdous@umass.edu>
2025-03-21 13:12:54 -0400

    Added test cases to both decision_coverage and statement_coverage regarding zero sides and negative sides of a triangle

--- test_suit/test_statementCoverage.py
+++ test_suit/test_statementCoverage.py
@@ -1,2 +1,14 @@
 import pytest
 from isTriangle import Triangle
+
+def test_zero_sides():
+  assert Triangle.classify(0,5,5) == Triangle.Type.INVALID
+  assert Triangle.classify(2,0,0) == Triangle.Type.INVALID
+  assert Triangle.classify(0,0,0) == Triangle.Type.INVALID
+  #tests that triangle with one or more zero sides is invalid triangle
+
+def test_negative_sides():
+  assert Triangle.classify(-2,3,3) == Triangle.Type.INVALID
+  assert Triangle.classify(-4,-4,4) == Triangle.Type.INVALID
+  assert Triangle.classify(-1,-1,-1) == Triangle.Type.INVALID
+  #tests that triangle with one or more negative sides is invalid triangle
